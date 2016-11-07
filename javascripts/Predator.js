2607cf351a9bccb6c7a4d859fa46dd1e5b831dcb
Nick Chemsak <nchemsak@gmail.com>
2016-11-06 18:50:38 -0600

    complete

--- javascripts/Predator.js
+++ javascripts/Predator.js
@@ -1,1 +1,27 @@
 'use strict';
+var Predator = (function(predator) {
+let carnivores;
+let herbivores;
+
+  predator.loadCarnivores = function(callbackToInvoke) {
+    let loader = new XMLHttpRequest();
+    loader.open("GET", "data/carnivores.json");
+    loader.send();
+    loader.addEventListener("load", function() {
+      carnivores = (JSON.parse(this.responseText)).carnivores;
+      callbackToInvoke("Carnivores", carnivores);
+
+    });
+  };
+  predator.loadHerbivores = function(callbackToInvoke) {
+    let loader = new XMLHttpRequest();
+    loader.open("GET", "data/herbivores.json");
+    loader.send();
+    loader.addEventListener("load", function() {
+
+      herbivores = (JSON.parse(this.responseText)).herbivores;
+      callbackToInvoke("Herbivores", herbivores);
+    });
+  };
+  return predator;
+})(Predator || {});
--- javascripts/iife-xhr.js
+++ javascripts/iife-xhr.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function show1(x, car) {
+  let carn = document.getElementById("carn");
+  carn.innerHTML += "<h2>" + x + "</h2>";
+  car.forEach(function(item) {
+    carn.innerHTML += "<li>" + item + "</li>";
+  });
+}
+
+function show2(y, her) {
+  let herb = document.getElementById("herb");
+  herb.innerHTML += "<h2>" + y + "</h2>";
+  her.forEach(function(item) {
+    herb.innerHTML += "<li>" + item + "</li>";
+  });
+}
+
+Predator.loadCarnivores(show1);
+Predator.loadHerbivores(show2);
